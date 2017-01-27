30814af6164acdc36fb358e569cb202f6aafb583
bluubloo <bloobluu@hotmail.com>
2017-01-27 15:15:30 +1300

    Changed score back to maximum tiles reached
    Have changed the score back to counting the maximum number of tiles reached. This number is now also counted correctly.

--- Android/ResponseTesting/src/uni/apps/responsetesting/fragment/events/MonkeyLadderFragment.java
+++ Android/ResponseTesting/src/uni/apps/responsetesting/fragment/events/MonkeyLadderFragment.java
@@ -229,6 +229,199 @@ public class MonkeyLadderFragment extends Fragment {
 		ActivityUtilities.displayResults(getActivity(), eventName, resultString);
 	}
 
+//constants
+	private static final String TAG = "MonkeyLadderFragment";
+	private static final String eventName = "Monkey Ladder";
+	private static final int maxTurns = 10;
+	private static final int gridColumns = 5;
+	private static final int waitTime = 250;
+	private static final int maxErrors = 3;
+
+	//views
+	private GridView grid;
+	private Button button;
+	private Button instruct;
+	
+	//varaibles
+	private int playTimes = 0;
+	private int tiles = 3;
+	private int maxTilesReached = tiles;
+	private int clickedTiles = 0;
+	private int errorCount = 0;
+	private int counter = 0;
+	private ArrayList<Integer> tileList;
+	private boolean clickable = false;
+
+	private DurationInfo[] results;
+	private MonkeyLadderGridAdapter adapter;
+	private Handler timerHandler = new Handler();
+
+	//-----------------------------------------------------------------------------
+	//RUNNABLES
+
+	private Runnable timerRunnableGridAppear = new Runnable(){
+
+		@Override
+		public void run() {
+			//sets start time
+			if(counter < maxTurns)
+				results[counter] = new DurationInfo(Calendar.getInstance().getTimeInMillis());
+			//show grid
+			grid.setVisibility(View.VISIBLE);
+			timerHandler.postDelayed(timerRunnableTextDisappear, tiles * 500);
+		}
+	};
+
+	private Runnable timerRunnableTextDisappear = new Runnable(){
+
+		@Override
+		public void run() {
+			//update adapter
+			adapter.update(false);
+			clickable = true;
+		}
+	};
+
+	//-----------------------------------------------------------------------------
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if(savedInstanceState != null)
+			playTimes = savedInstanceState.getInt("playTime");
+		setRetainInstance(true);
+		setUpTiles();
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState){
+		super.onSaveInstanceState(outState);
+		outState.putInt("playTime", playTimes);
+	}
+
+	@Override
+	public void onStop(){
+		super.onStop();
+		removeTimerCallbacks();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+			Bundle savedInstanceState) {
+		Log.d(TAG, "onCreateView");
+		// Inflate the layout for this fragment
+		View view =  inflater.inflate(R.layout.monkey_ladder_fragment, container, false);
+		//sets views
+		grid = (GridView) view.findViewById(R.id.monkey_grid);
+		button = (Button) view.findViewById(R.id.button_start);
+		instruct = (Button) view.findViewById(R.id.button_instruct);
+		//sets adapter
+		adapter = new MonkeyLadderGridAdapter(getActivity(), tileList, tiles);
+		grid.setAdapter(adapter);
+		setUpButton();
+		setUpGridItemClick();
+		return view;
+	}
+
+	private void setUpGridItemClick() {
+		//sets grid click
+		grid.setOnItemClickListener(new OnItemClickListener(){
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				//if clickable
+				if(clickable){	
+					//check click result
+					if((int) adapter.getItem(position) <= tiles){
+						boolean result = checkClick(position);
+						if(result){
+							clickedTiles ++;
+							if(clickedTiles >= tiles){
+								clickable = false;
+								if(counter < maxTurns - 1)
+									next(true);
+								else
+									endTest();
+							} else{
+								adapter.update();
+							}						
+						}else{
+							clickable = false;
+							errorCount ++;
+							if(errorCount == maxErrors)
+								endTest();
+							else{
+								next(false);
+							}						
+						}
+					}
+				}
+			}
+		});
+	}
+
+	private void setUpButton() {
+		//set start button click
+		button.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				if(ActivityUtilities.checkPlayable(eventName, playTimes, getActivity())){
+					if(button.isEnabled()){
+						//set initial test variables
+						button.setEnabled(false);
+						instruct.setEnabled(false);
+						playTimes++;
+						counter = 0;
+						tiles = 3;
+						maxTilesReached = tiles;
+						clickedTiles = 0;
+						errorCount = 0;
+						clickable = false;
+						results = new DurationInfo[maxTurns];
+						results[counter] = new DurationInfo(Calendar.getInstance().getTimeInMillis());
+						//alters tiles
+						alterTiles(false);
+						grid.setVisibility(View.VISIBLE);
+						timerHandler.postDelayed(timerRunnableTextDisappear, tiles * 500);
+					}
+				}else{
+					ActivityUtilities.displayResults(getActivity(), eventName,
+							"You have completed you daily 3 tries, please try a different test");
+				}
+			} 
+
+		});
+		
+		instruct.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				ActivityUtilities.eventInfo(eventName, getActivity());
+			}
+			
+		});
+	}
+
+	//ends test
+	private void endTest(){
+		//reset variables
+		removeTimerCallbacks();
+		button.setEnabled(true);
+		instruct.setEnabled(true);
+		grid.setVisibility(View.INVISIBLE);
+		//gets results
+		String time = getResult();
+		String resultString = Integer.toString(maxTilesReached) + " reached. " + time + " average time (s).";
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		String userId = prefs.getString(getResources().getString(R.string.pref_key_user_id), "single");
+		//inserts and displays results
+		Results.insertResult(eventName, Integer.toString(maxTilesReached) + "|" + time,
+				Calendar.getInstance().getTimeInMillis(), getActivity(), userId);
+		ActivityUtilities.displayResults(getActivity(), eventName, resultString);
+	}
+
 	//gets results as string
 	private String getResult() {
 		double tmp = 0;
@@ -253,6 +446,9 @@ public class MonkeyLadderFragment extends Fragment {
 	private void alterTiles(boolean inc){
 		if(inc)
 			tiles ++;
+			if(tiles > maxTilesReached){
+				maxTilesReached ++;
+			}
 		else if(tiles > 3)
 			tiles --;
 		//shuffles list then updates adapter
